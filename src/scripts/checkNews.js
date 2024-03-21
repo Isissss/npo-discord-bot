6911f46fb5c583c09509298ea6e084db5fdb1fcc
campingstoel <cterlouw04@gmail.com>
2024-03-21 20:33:46 +0100

    fix(heroku): puppeteer now works as expected

--- src/scripts/checkNews.js
+++ src/scripts/checkNews.js
@@ -1,67 +1,61 @@
 import {
-    Client,
-    GatewayIntentBits,
-    PermissionsBitField,
-    Routes,
-    ActivityType,
-    SlashCommandBuilder,
-    Collection,
-    ChannelType,
-    ButtonBuilder,
-    EmbedBuilder,
-    ActionRowBuilder,
-    ButtonStyle,
-  } from "discord.js";
-  import fs from "fs";
-  import {fetchNewsFromNos} from "../api/scrapeNews.js";
+  Client,
+  GatewayIntentBits,
+  PermissionsBitField,
+  Routes,
+  ActivityType,
+  SlashCommandBuilder,
+  Collection,
+  ChannelType,
+  ButtonBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} from "discord.js";
+import fs from "fs";
+import { fetchNewsFromNos } from "../api/scrapeNews.js";
+import crypto from "crypto";
 
+export async function checkNews(channel) {
+  const hashes = JSON.parse(fs.readFileSync("./hashes.json"));
 
+  const [national, sport, international] = await Promise.all([
+    fetchNewsFromNos("https://nos.nl/nieuws/binnenland", "Binnenland"),
+    fetchNewsFromNos("https://nos.nl/sport/laatste", "Sport"),
+    fetchNewsFromNos("https://nos.nl/nieuws/buitenland", "Buitenland"),
+  ]);
 
- export async function checkNews(channel) {
-    const hashes = JSON.parse(fs.readFileSync('./hashes.json')); 
+  const sendNews = (data, tag) => {
+    const hash = crypto.createHash("sha256").update(data.text).digest("hex");
+    const exists = hashes.find((h) => h === hash);
+    if (exists) return console.log("Hash found");
+    hashes.push(hash);
+    const role = channel.guild.roles.cache.find((r) => r.name === tag);
 
+    const newsEmbed = new EmbedBuilder()
+      .setTitle(tag)
+      .setColor(0xffa500)
+      .setDescription(data.text)
+      .setImage(data.src)
+      .setURL(data.href)
+      .setTimestamp();
 
-    const [national, sport, international] = await Promise.all([
-        fetchNewsFromNos('https://nos.nl/nieuws/binnenland', 'Binnenland'),
-        fetchNewsFromNos('https://nos.nl/sport/laatste', 'Sport'),
-        fetchNewsFromNos('https://nos.nl/nieuws/buitenland', 'Buitenland')
-    ]);
+    const newsButton = new ButtonBuilder()
+      .setLabel("Open artikel")
+      .setStyle(ButtonStyle.Link)
+      .setURL(data.href);
 
-    const sendNews = (data, tag) => {
-        const hash = crypto.createHash('sha256').update(data.text).digest('hex');
-        const exists = hashes.find(h => h === hash);
-        if (exists) return console.log("Hash found");
-        hashes.push(hash);
-        const role = channel.guild.roles.cache.find(r => r.name === tag);
+    const newsActionRow = new ActionRowBuilder().addComponents(newsButton);
 
-        const newsEmbed = new EmbedBuilder()
-            .setTitle(tag) 
-            .setColor(0xffa500)
-            .setDescription(data.text)
-            .setImage(data.src)
-            .setURL(data.href)
-            .setTimestamp();
+    channel.send({
+      content: `${role.toString()}`,
+      embeds: [newsEmbed],
+      components: [newsActionRow],
+    });
+    fs.writeFileSync("./hashes.json", JSON.stringify(hashes));
+  };
 
-        const newsButton = new ButtonBuilder()
-            .setLabel("Open artikel")
-            .setStyle(ButtonStyle.Link)
-            .setURL(data.href);
-
-        const newsActionRow = new ActionRowBuilder().addComponents(newsButton);
-
-        channel.send({
-            content: `${role.toString()}`,
-            embeds: [newsEmbed],
-            components: [newsActionRow]
-        });
-        fs.writeFileSync('./hashes.json', JSON.stringify(hashes));
-    };
-
-    sendNews(national, national.tag);
-    sendNews(sport, sport.tag);
-    sendNews(international, international.tag);
-
-};
-
-
-
+  sendNews(national, national.tag);
+  sendNews(sport, sport.tag);
+  sendNews(international, international.tag);
+}
